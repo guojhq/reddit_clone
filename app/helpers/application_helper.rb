2a69bf901db79b8865faa900af9e0a846181d4e8
spicypig <spicypig@users.noreply.github.com>
2014-11-23 15:36:49 -0800

    Added a helper method my_name in  ApplicationHelper module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # good for truly global functionality as helpers are avilable in all views.
+  # for page-specifig code, use another way!!
+  def my_name
+    "Chuan He"
+  end
 end
