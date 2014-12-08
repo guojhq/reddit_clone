5a0ceb1d3375231d11d25b1307b25bd9fb277932
spicypig <spicypig@users.noreply.github.com>
2014-12-07 20:43:44 -0800

    Added User and Post association, Updated Seeds, Adding Author information and thea Scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
+
+ # The default scope declaration above will order all posts by their created_at date, in descending order.
+  default_scope { order('created_at DESC') }
 end
