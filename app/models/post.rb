be2ec71fa24421637cacb6b7b208d9761bfe9fb0
spicypig <spicypig@users.noreply.github.com>
2014-12-08 23:33:04 -0800

    Added authorization using Pundit Gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
 
  # The default scope declaration above will order all posts by their created_at date, in descending order.
   default_scope { order('created_at DESC') }
+  #scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+  #scope :ordered_by_title, -> { order('title')}
 end
