a935ef6f50c24a1bda9b7f82be6a608fef203648
spicypig <spicypig@users.noreply.github.com>
2014-12-11 14:40:08 -0800

    Associated and nested topics and posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-
+  belongs_to :topic
  # The default scope declaration above will order all posts by their created_at date, in descending order.
   default_scope { order('created_at DESC') }
   #scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
