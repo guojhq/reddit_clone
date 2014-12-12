39ba3ba595fc5dfc5a6660e6fad7475f3f278aa0
spicypig <spicypig@users.noreply.github.com>
2014-12-11 16:55:05 -0800

    Added Post validations, added post form partial to make the code DRY, used Application Helper to refactor the form partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
   # good for truly global functionality as helpers are avilable in all views.
   # for page-specifig code, use another way!!
-  def my_name
-    "Chuan He"
+
+  #  The 1st argument is an array of errors, and the 2nd is a block
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   belongs_to :topic
- # The default scope declaration above will order all posts by their created_at date, in descending order.
+  # The default scope declaration above will order all posts by their created_at date, in descending order.
   default_scope { order('created_at DESC') }
-  #scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
-  #scope :ordered_by_title, -> { order('title')}
+  # scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+  # scope :ordered_by_title, -> { order('title')}
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
