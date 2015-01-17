96dd25c670568919ab2fdf859db8a7af70651238
spicypig <spicypig@users.noreply.github.com>
2015-01-17 15:54:07 -0800

    Added Popular Posts and Active Users, Implemented early loading to reduce the ActiveRecord query time

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,84 @@
+class Topics::PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @comments = @post.comments
+    @comment = Comment.new
+    authorize @topic
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    # user specific post
+    if save_with_initial_vote
+      flash[:notice] = "Post was saved."
+      redirect_to [@post.topic, @post] 
+    else 
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes
+      flash[:notice] = "Post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+
+   def destroy
+      @topic = Topic.find(params[:topic_id])
+      @post = Post.find(params[:id])
+      title = @post.title
+
+      authorize @post
+      if @post.destroy
+        flash[:notice] = "\"#{title}\" was deleted successfully."
+        redirect_to @topic
+      else
+        flash[:error] = "There are an error deleting the topic."
+        render :show
+      end
+   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+
+private
+
+  def save_with_initial_vote
+    ActiveRecord::Base.transaction do 
+      @post = current_user.posts.build(post_params)
+      @topic = Topic.find(params[:topic_id])
+      @post.topic = @topic
+      authorize @post
+      if @post.save
+        @post.create_vote
+      end
+    end
+    @post.present?
+  end
+
+end
